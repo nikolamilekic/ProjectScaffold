53370b570a2f6addc1c5c7c6a65f8a7e7f0f7135
Nikola Milekic <nikolamilekic@gmail.com>
2017-02-25 09:06:15 +0100

    Replace NUnit project with Expecto

--- tests/FSharp.ProjectTemplate.Tests/Tests.fs
+++ tests/FSharp.ProjectTemplate.Tests/Tests.fs
@@ -1,10 +1,15 @@
 ﻿module FSharp.ProjectScaffold.Tests
 
-open FSharp.ProjectTemplate
-open NUnit.Framework
+open Expecto
 
-[<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+[<Tests>]
+let tests =
+    testList "samples" [
+        testCase "universe exists" <| fun _ ->
+            let subject = true
+            Expect.isTrue subject "I compute, therefore I am."
+
+        testCase "should fail" <| fun _ ->
+            let subject = false
+            Expect.isTrue subject "I should fail because the subject is false."
+    ]
